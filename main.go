14bc17b8d137bd35d1693eba01a801730dbf098b
Ezequiel <ezequiel.maranda@gmail.com>
2024-04-01 22:43:58 -0300

    tasks routes created

--- main.go
+++ main.go
@@ -19,10 +19,17 @@ func main() {
 
 	r.HandleFunc("/", routes.HomeHandler)
 
+	// Users routes
 	r.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
 	r.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
 	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
 	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+
+	// Tasks routes
+	r.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks", routes.CreateTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
 
 	http.ListenAndServe(":3000", r)
 }
--- routes/tasks.routes.go
+++ routes/tasks.routes.go
@@ -1,1 +1,61 @@
 package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/echudev/go-gorm-restapi/db"
+	"github.com/echudev/go-gorm-restapi/models"
+	"github.com/gorilla/mux"
+)
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	// el metodo Find recibe el puntero de la variable tasks
+	// para guardar en ella los valores reibidos de la base de datos
+	db.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(tasks)
+}
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	json.NewDecoder(r.Body).Decode(&task)
+	createdTask := db.DB.Create(&task)
+
+	err := createdTask.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) //400
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound) //400
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound) //400
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	db.DB.Unscoped().Delete(&task)
+	w.WriteHeader(http.StatusNoContent) //204 - no hay nada que devolver pero está ok
+}
